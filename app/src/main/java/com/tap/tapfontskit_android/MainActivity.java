f49b78e319e3cdfcde3077a3b5c89708c3c739e4
Haitham Elsheshtawy <h.sheshtawy@tap.company>
2019-02-04 19:20:36 +0300

    TapFontKit
    First Launch

---
+++ app/src/main/java/com/tap/tapfontskit_android/MainActivity.java
@@ -0,0 +1,58 @@
+package com.tap.tapfontskit_android;
+
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        final TextView fontTextView = this.findViewById(R.id.font);
+        final String font = sharedPreferences.getString(this.getString(R.string.preference_font_face), this.getString(R.string.font_face_default_value));
+
+        fontTextView.setTypeface(Typeface.createFromAsset(this.getAssets(), font));
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final TextView fontTextView =  this.findViewById(R.id.font);
+        final String font = sharedPreferences.getString(this.getString(R.string.preference_font_face), this.getString(R.string.font_face_default_value));
+
+        fontTextView.setTypeface(Typeface.createFromAsset(this.getAssets(), font));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        this.getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        final int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            this.startActivity(SettingsActivity.start(this));
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/tap/tapfontskit_android/SettingsActivity.java
@@ -0,0 +1,59 @@
+package com.tap.tapfontskit_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public static Intent start(final Activity activity) {
+        return new Intent(activity, SettingsActivity.class);
+    }
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.setContentView(R.layout.frame_layout);
+        this.getFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
+          private Preference fontFacePref;
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            this.addPreferencesFromResource(R.xml.preferences);
+
+              fontFacePref =  this.findPreference("preference_font_face");
+              fontFacePref.setOnPreferenceChangeListener(this);
+
+            this.getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceChange(final Preference preference, final Object newValue) {
+
+            if (preference.equals(fontFacePref)) {
+               // final String value = (String) newValue;
+                //Toast.makeText(getActivity(), "New value is " + value, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
+            if (fontFacePref != null && key.equals(fontFacePref.getKey())) {
+                final String value = sharedPreferences.getString(key, "");
+                fontFacePref.setSummary(value);
+            }
+        }
+    }
+}
